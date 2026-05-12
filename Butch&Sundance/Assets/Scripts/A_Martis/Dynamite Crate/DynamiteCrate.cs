328bd41fc18c40db9653e07b9e51c3ba26c3adb3
AmirMartisFSO <AAMartis@student.fullsail.edu>
2026-05-12 18:08:01 -0400

    Dynamite Crate Update
    Mechanics and comments added

--- Butch&Sundance/Assets/Scripts/A_Martis/Dynamite Crate/DynamiteCrate.cs
+++ Butch&Sundance/Assets/Scripts/A_Martis/Dynamite Crate/DynamiteCrate.cs
@@ -1,29 +1,39 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;   
 
 public class DynamiteCrate : MonoBehaviour, I_Damage
 {
+    // HP initializers
     [SerializeField] int crateMaxHP;
     int crateCurHP;
 
+    // Explosion initializers
     [SerializeField] int explodeRadius;
     [SerializeField] int explodeDamage;
 
+    // Prevents crate from exploding multiple times
     bool exploded;
 
+    // Sets crate's HP to value set in Unity
     void Awake()
     {
         crateCurHP = crateMaxHP;
     }
 
+    // Damage function that I_Damage uses
     public void TakeDamage(int Amount, string BodyPart)
     {
+        // Prevents additional damage after explosion
         if (exploded)
         {
             return;
         }
 
+        // Crate takes damage
         crateCurHP -= Amount;
 
+        // Run Explode() if crate takes enough damage
         if(crateCurHP <=0)
         {
             Explode();
@@ -34,21 +44,33 @@ public class DynamiteCrate : MonoBehaviour, I_Damage
     {
         exploded = true;
 
+        // Creates an invisible area that detects nearby colliders
         Collider[] Hits = Physics.OverlapSphere(transform.position, explodeRadius);
 
+        // Tracks objects that have already been damaged to prevent multi-hit
+        List<I_Damage> DamagedObjects = new List<I_Damage>();
+
+        // Loops through every collider in "Hits" radius
         foreach (Collider c in Hits)
         {
+            // Force ignore the crate itself
             if (c.gameObject == gameObject) continue;
 
-            I_Damage damage = c.GetComponent<I_Damage>();
-
-            if (damage != null)
+            // Search parent hierarchy for correct object to damage to prevent multi-hit
+            I_Damage damage = c.GetComponentInParent<I_Damage>();
+            
+            // Checks if an object can be damage and hasn't been damaged yet
+            if (damage != null && !DamagedObjects.Contains(damage))
             {
+                // Applies explosion damage to the object
                 damage.TakeDamage(explodeDamage, "Body");
+
+                // Adds the damaged object to the list to prevent multi-hit
+                DamagedObjects.Add(damage);
             }
-            Debug.Log(c.name);
         }
 
+        // Removes the crate
         Destroy(gameObject);
     }
 }
