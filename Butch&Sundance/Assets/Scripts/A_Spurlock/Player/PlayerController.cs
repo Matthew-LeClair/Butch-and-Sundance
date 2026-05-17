ab5ee813a6366c1ed22c4cc20059d3c2d9cd9c9d
MadTeaPartyDevelopment <madteapartydevelopment@outlook.com>
2026-05-17 13:19:09 -0500

    Added Wall Run & Jump
    Whoever Presents in a couple weeks, PLEASE practice with the Parkour Mechanics.

--- Butch&Sundance/Assets/Scripts/A_Spurlock/Player/PlayerController.cs
+++ Butch&Sundance/Assets/Scripts/A_Spurlock/Player/PlayerController.cs
@@ -39,6 +39,28 @@ public class PlayerController : MonoBehaviour, I_Damage
     float JumpSpeed;
     int JumpCount;
 
+    [Header("Parkour")]
+    // Wallrun
+    public LayerMask WhatIsWall;
+    public LayerMask WhatIsGround;
+    public float WallRunForce;
+    public float MaxWallRunTime;
+    public float UpJumpForce;
+    public float OutJumpForce;
+    private float WallRunTimer;
+    private bool IsWallRunning;
+
+    public float WallCheckDistance;
+    public float MinJumpHeight;
+    private RaycastHit LeftWall;
+    private bool IsLeftWall;
+    private RaycastHit RightWall;
+    private bool IsRightWall;
+    private bool UseGravity;
+
+
+
+
 
     [Header("Momemtum")]
     [SerializeField] public float BaseMomentumBuildRate;
@@ -77,11 +99,11 @@ public class PlayerController : MonoBehaviour, I_Damage
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         IsMoving = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f
             || Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f;
-
+        
         Movement(); // Moves once per frame for better smoothing
     }
 
@@ -102,7 +124,7 @@ public class PlayerController : MonoBehaviour, I_Damage
 
         if (Controller.isGrounded)
         {
-            PlayerVel.y = 0;
+            PlayerVel = new Vector3(0, 0, 0); // Reset all axes on landing
             JumpCount = 0; // Reset Jump Count on Landing
         }
 
@@ -110,18 +132,23 @@ public class PlayerController : MonoBehaviour, I_Damage
             Input.GetAxis("Horizontal") * transform.right
             + Input.GetAxis("Vertical") * transform.forward;
 
-        if (InputDir.magnitude > 0.1f) // If there is Input...
+        HandleWallRun();
+        
+        if (!IsWallRunning)
         {
-            Moved = true;
-            MoveDir = InputDir; // Only overwrite MoveDir when actively moving
-            MomentumVelocity = MoveDir.normalized * (Speed + CurrMomentum * 0.1f); // Snap directly to Input Direction
-        }
-        else // If there is No Input...
-        {
-            MomentumVelocity = Vector3.MoveTowards(
-                MomentumVelocity,
-                Vector3.zero,
-                Speed * 10f * Time.deltaTime); // Quickly bleed to zero on no input
+            if (InputDir.magnitude > 0.1f) // If there is Input...
+            {
+                Moved = true;
+                MoveDir = InputDir; // Only overwrite MoveDir when actively moving
+                MomentumVelocity = MoveDir.normalized * (Speed + CurrMomentum * 0.1f); // Snap directly to Input Direction
+            }
+            else // If there is No Input...
+            {
+                MomentumVelocity = Vector3.MoveTowards(
+                    MomentumVelocity,
+                    Vector3.zero,
+                    Speed * 10f * Time.deltaTime); // Quickly bleed to zero on no input
+            }
         }
 
         HandleMomentum(InputDir); // Pass Input Direction to Momentum System
@@ -131,22 +158,25 @@ public class PlayerController : MonoBehaviour, I_Damage
         Jump();
 
         Controller.Move(PlayerVel * Time.deltaTime);
-        PlayerVel.y -= Gravity * Time.deltaTime;
+        if (UseGravity) { PlayerVel.y -= Gravity * Time.deltaTime; }
     }
 
     void Jump()
     {
         if (Input.GetButtonDown("Jump") && JumpCount < 2) // Allow Double Jump
         {
-            Jumped = true;
-            JumpCount++;
+            if (!IsWallRunning)
+            {
+                Jumped = true;
+                JumpCount++;
 
-            float MomentumBoost = CurrMomentum / MaxMomentum; // Momentum as a Percentage
-            PlayerVel.y = JumpSpeedBase * (1f + MomentumBoost * 0.4f); // Scale Jump Height with Momentum
+                float MomentumBoost = CurrMomentum / MaxMomentum; // Momentum as a Percentage
+                PlayerVel.y = JumpSpeedBase * (1f + MomentumBoost * 0.4f); // Scale Jump Height with Momentum
 
-            MomentumVelocity = new Vector3(
-                MomentumVelocity.x, 0, MomentumVelocity.z) // Strip Y from Momentum Velocity
-                * (1f + MomentumBoost * 0.3f); // Boost Horizontal on Jump
+                MomentumVelocity = new Vector3(
+                    MomentumVelocity.x, 0, MomentumVelocity.z) // Strip Y from Momentum Velocity
+                    * (1f + MomentumBoost * 0.3f); // Boost Horizontal on Jump
+            } else { WallJump(); }
         }
     }
 
@@ -254,5 +284,64 @@ public class PlayerController : MonoBehaviour, I_Damage
         yield return new WaitForSeconds(0.5f); // Wait for a short duration (0.5 seconds) before deactivating the damage screen effect
 
         GameManager.Instance.PlayerDamage_Screen.SetActive(false); // Deactivate the damage screen effect after the wait time has elapsed
+    }
+
+
+
+    void CheckForWall()
+    {
+        IsRightWall = Physics.Raycast(transform.position, transform.right, out RightWall, WallCheckDistance, WhatIsWall); // Check for Wall on Right
+        IsLeftWall = Physics.Raycast(transform.position, -transform.right, out LeftWall, WallCheckDistance, WhatIsWall); // Check for Wall on Left
+    }
+
+    bool AboveGround()
+    { return !Physics.Raycast(transform.position, Vector3.down, MinJumpHeight, WhatIsGround); } // Check if Above Ground
+
+    void HandleWallRun()
+    {
+        // Step 1 -- Wall Run Timer
+        if (WallRunTimer >= MaxWallRunTime) { IsWallRunning = false; WallRunTimer = 0; }
+        {
+            WallRunTimer += Time.deltaTime;
+            
+            // Step 2 -- Wall Check
+            CheckForWall();
+
+            // Step 3 -- Wall Run Check
+            if ((IsRightWall || IsLeftWall) && MoveDir.magnitude > 0.1f && AboveGround() && !Controller.isGrounded)
+            { IsWallRunning = true; UseGravity = false; }
+            else { IsWallRunning = false; UseGravity = true; }
+
+            // Step 4 -- Wall Run
+            if (IsWallRunning) { WallRunMovement(); }
+        }
+    }
+
+    void WallRunMovement()
+    {
+        UseGravity = false; // Cancel Gravity while Wall Running
+
+        Vector3 Normal = IsRightWall ? RightWall.normal : LeftWall.normal; // Get Wall Normal
+        Vector3 WallForward = Vector3.Cross(Normal, transform.up); // Calculate Wall Forward Direction
+
+        if ((transform.forward - WallForward).magnitude > (transform.forward - -WallForward).magnitude)
+        { WallForward = -WallForward; }
+
+        if (Vector3.Dot(WallForward, transform.forward) < 0) // If Wall Forward is behind the Player...
+        { WallForward = -WallForward; } // Flip Wall Forward Direction
+
+        float VerticalDot = Vector3.Dot(Camera.main.transform.forward, Vector3.up); // How much Camera is pointing Up or Down (-1 to 1)
+        PlayerVel.y = VerticalDot * Speed;
+    }
+
+    void WallJump()
+    {
+        Vector3 Normal = IsRightWall ? RightWall.normal : LeftWall.normal; // Get Wall Normal
+        Vector3 ForceToApply = transform.up * UpJumpForce + Normal * OutJumpForce; // Calculate Jump Force
+
+        PlayerVel.y = 0f; // Reset Y Velocity before Jump
+        PlayerVel = ForceToApply; // Apply Jump Force to Player Velocity
+        MomentumVelocity = Vector3.zero;
+        IsWallRunning = false; // Stop Wall Running
     }
 }
