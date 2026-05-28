8d69c6f4373ae243d6a38f88b28032aaecc6136d
MadTeaPartyDevelopment <madteapartydevelopment@outlook.com>
2026-05-28 08:29:25 -0500

    Fixed Tutorial... I hope.

---
+++ Butch&Sundance/Assets/Scripts/A_Spurlock/TutorialText.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour
+{
+    [Header("Text")]
+    [SerializeField] public string Text;
+    [SerializeField] public GameObject TutorialTxt;
+    [SerializeField] public TMP_Text UI_Text;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TutorialTxt.gameObject.SetActive(true);
+        UI_Text.text = Text;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TutorialTxt.gameObject.SetActive(false);
+    }
+}
