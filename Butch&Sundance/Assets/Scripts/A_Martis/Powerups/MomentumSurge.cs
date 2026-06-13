fbd539a6b1ef8fbbdb7e0e8995f1e8dff516196a
AmirMartisFSO <AAMartis@student.fullsail.edu>
2026-06-12 20:01:57 -0400

    Momentum Surge Visual Effects
    As description

--- Butch&Sundance/Assets/Scripts/A_Martis/Powerups/MomentumSurge.cs
+++ Butch&Sundance/Assets/Scripts/A_Martis/Powerups/MomentumSurge.cs
@@ -4,12 +4,17 @@ public class MomentumSurge : PowerupBase
 {
     [SerializeField] float momentumMultiplier;
     [SerializeField] float speedBonus;
+    [SerializeField] float jumpBonus;
 
     [SerializeField] AudioClip pickupSound;
     [SerializeField, Range(0f, 1f)] float volume;
 
+    [SerializeField] GameObject powerupVFX;
+    GameObject vfxInstance;
+
     float originalMomentum;
     float originalSpeed;
+    float originalJump;
 
     protected override void ApplyEffect(PlayerController player)
     {
@@ -20,9 +25,24 @@ public class MomentumSurge : PowerupBase
 
         originalMomentum = player.MomentumBuildRate;
         originalSpeed = player.SpeedBase;
+        originalJump = player.JumpSpeedBase;
 
         player.MomentumBuildRate *= momentumMultiplier;
         player.SpeedBase += speedBonus;
+        player.JumpSpeedBase += jumpBonus;
+
+        if(powerupVFX != null && vfxInstance == null)
+        {
+            vfxInstance = Instantiate(powerupVFX, player.CamTransform);
+        }
+
+        if (vfxInstance != null)
+        {
+            vfxInstance.SetActive(true);
+            var ps = vfxInstance.GetComponent<ParticleSystem>();
+            if (ps != null) ps.Play();
+        }
+
         GameManager.Instance.PlayerMomentum.SetActive(true);
     }
 
@@ -30,6 +50,16 @@ public class MomentumSurge : PowerupBase
     {
         player.MomentumBuildRate = originalMomentum;
         player.SpeedBase = originalSpeed;
+        player.JumpSpeedBase = originalJump;
+
+        if(powerupVFX != null)
+        {
+            var ps = vfxInstance.GetComponent<ParticleSystem>();
+            if (ps != null) ps.Stop();
+            Destroy(vfxInstance);
+            vfxInstance= null;
+        }
+
         GameManager.Instance.PlayerMomentum.SetActive(false);
     }
 }
